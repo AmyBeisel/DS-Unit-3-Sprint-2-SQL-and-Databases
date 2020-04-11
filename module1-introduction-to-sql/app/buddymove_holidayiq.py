812aef976e7495a9c5b81c252b4e27c53bb65e67
Amy Beisel <amy.beisel@gmail.com>
2020-04-10 21:28:06 -0600

    part 2

--- module1-introduction-to-sql/app/buddymove_holidayiq.py
+++ module1-introduction-to-sql/app/buddymove_holidayiq.py
@@ -6,6 +6,26 @@ import pandas as pd
 
 
 df = pd.read_csv('./data/buddymove_holidayiq.csv')
-df.rename(columns = {'User Id': 'User_Id'}, inplace = True)
+
 conn = sq.connect('buddymove_holidayiq.sqlite3')
 df.to_sql('buddymove_holiday', conn)
+curs = conn.cursor()
+
+
+ #how many rows do I have?
+query = """
+SELECT count(distinct "User Id") as user_count
+FROM buddymove_holiday"""
+
+result = curs.execute(query).fetchone()
+print("I have this many rows -", result)
+
+#How many users who reviewed at least 100 `Nature` in the category also
+#reviewed at least 100 in the `Shopping` category?
+
+query1 = """SELECT count(DISTINCT "User Id") as user_count
+FROM buddymove_holiday as bh 
+WHERE bh.Nature >=100 and bh.Shopping >=100"""
+
+result1 = curs.execute(query).fetchone()
+print("this many users reviewed Nature and Shopping category -", result1)
